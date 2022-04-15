e5f162b04880773cf85a9124bc1e0c8a4c9b5acb
haderman <hadermania@gmail.com>
2022-04-15 17:35:08 -0500

    add global mute button

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+let state = {
+  muted: true,
+};
+
 document
   .querySelectorAll('video')
   .forEach(showControls);
@@ -8,9 +12,12 @@ const observer = new MutationObserver(([mutation]) => {
     .forEach(node =>
       node
         .querySelectorAll('video')
-        .forEach(showControls)
+        .forEach($video => {
+          showControls($video);
+          setMutedState($video);
+        })
   );
-}, config);
+});
 
 observer.observe(
   document.getElementById('list-view-2'),
@@ -20,3 +27,41 @@ observer.observe(
 function showControls($video) {
   $video.controls = true;
 }
+
+function setMutedState($video) {
+  $video.muted = state.muted;
+}
+
+// add global muted button
+
+document
+  .querySelector('body')
+  .appendChild(createButton())
+
+function createButton() {
+  const button = document.createElement('button');
+
+  button.style.position = 'fixed';
+  button.style.zIndex = 10;
+  button.style.bottom = '20px';
+  button.style.left = '20px';
+  button.style.backgroundColor = 'black';
+  button.style.borderRadius = '50%';
+  button.style.padding = '24px';
+  button.style.border = '3px solid hsl(210, 40%, 50%)';
+  button.style.cursor = 'pointer';
+
+  // 9gag css class
+  button.className = state.muted ? 'sound-toggle off' : 'sound-toggle on';
+
+  button.addEventListener('click', () => {
+    state.muted = !state.muted;
+    button.className = state.muted ? 'sound-toggle off' : 'sound-toggle on';
+    document
+      .querySelectorAll('video')
+      .forEach(setMutedState);
+  });
+
+  return button;
+}
+
